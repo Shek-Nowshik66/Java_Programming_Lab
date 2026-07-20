3de572416950b97d9ae9001e4860075f438d883c
laptop-0rkmoni3 user <laptop-0rkmoni3.user@default.com>
2026-07-20 19:38:24 +0530

    adds 2 more programs in week3

---
+++ Week3/AddMat.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class AddMat {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int a[][] = new int[2][2];
+    int b[][] = new int[2][2];
+    int c[][] = new int[2][2];
+    for (int i = 0; i < 2; i++) {
+      for(int j = 0; j < 2; j++)
+        a[i][j] = s.nextInt();
+    }
+    for (int i = 0; i < 2; i++) {
+      for(int j = 0; j < 2; j++)
+        b[i][j] = s.nextInt();
+    }
+    for (int i = 0; i < 2; i++) {
+      for(int j = 0; j < 2; j++)
+        c[i][j] = a[i][j] + b[i][j];
+    }
+    for (int i = 0; i < 2; i++) {
+      for(int j = 0; j < 2; j++)
+        System.out.print(c[i][j] + (j == 2 - 1 ? "" : " "));
+      System.out.println();
+    }
+    s.close();
+  }
+}
